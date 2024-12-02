2d55f5e244c72670411d1902c5bbb740d64957ae
Alexander Klimov <alexandrklimov04@gmail.com>
2024-12-03 06:36:06 +0700

    create database file

---
+++ src/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+from config import settings
+
+engine = create_async_engine(settings.DB.get_postgres_dsn, future=True, echo=True)
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+async def get_db():
+    async with async_session() as session:
+        yield session
